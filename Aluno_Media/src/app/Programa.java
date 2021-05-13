5cb90b086ff33be94b37cb5b359f8efd1d8d1b62
Anderson <anderson.p20@gmail.com>
2021-05-13 16:12:03 -0300

    Primeiro commit

---
+++ Aluno_Media/src/app/Programa.java
@@ -0,0 +1,40 @@
+package app;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entidades.Aluno;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Aluno aluno = new Aluno();
+		
+		System.out.print("Digiti o nome do Aluno: ");
+		aluno.nome = sc.nextLine();
+		System.out.println("Digiti as três notas do aluno:");
+		System.out.print("Nota 1: ");
+		aluno.nota1 = sc.nextDouble();
+		System.out.print("Nota 2: ");
+		aluno.nota2 = sc.nextDouble();
+		System.out.print("Nota 3: ");
+		aluno.nota3 = sc.nextDouble();
+		
+		System.out.printf("Nota Final: %.2f%n", aluno.notaFinal());
+		
+		if (aluno.notaFinal() < 60.0) {
+			System.out.println("Reprovado");
+			System.out.printf("Faltam %.2f pontos para atingir a média %n", aluno.pontosEmFalta());
+		} else {
+			System.out.println("Aprovado");
+		}
+		
+		sc.close();
+
+	}
+
+}
---
+++ Aluno_Média.md
@@ -0,0 +1,19 @@
+Fazer um programa para ler o nome de um aluno e as três notas que ele obteve nos três trimestres do ano
+(primeiro trimestre vale 30 e o segundo e terceiro valem 35 cada). Ao final, mostrar qual a nota final do aluno no ano. Dizer também se o aluno está **APROVADO** ou **REPROVADO** e, em caso negativo, quantos pontos faltam para o aluno obter o mínimo para ser aprovado (que é 60% da nota). Você deve criar uma classe Aluno para resolver este problema.
+
+
+
+Exemplos:
+Entrada: 																								Saída:
+Anderson Pinto																					Nota Final: 90.00
+27.00																									 Aprovado
+31.00
+32.00
+
+
+
+Entrada: 																								Saída:
+Alex Silva  																							Nota Final: 52.00
+17.00																									 Reprovado 
+20.00																									 Faltam 8.00 pontos para atingir a média
+15.00
